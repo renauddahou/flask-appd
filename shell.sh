1a7cd3501f2abe9a4523a660e3b319df67fdbd98
renaudatascience <85571576+renauddahou@users.noreply.github.com>
2021-12-05 12:51:56 +0100

    Add files via upload

---
+++ shell.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+yum update -y
+yum install python-pip -y;
+pip install flask; spacy==2.3.5; gensim==3.6.0; https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-2.3.1/en_core_web_sm-2.3.1.tar.gz; docx2txt; python-docx; pdfminer; deep_translator; pyresparser; nltk; textdistance; pymysql; flask-mysql;
+
